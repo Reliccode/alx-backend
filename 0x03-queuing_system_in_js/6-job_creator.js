6b727d2b43dec158cf101c6c8902a0f182052bc5
Reliccode <bryanmaritim@gmail.com>
2024-03-06 15:29:43 +0300

    fix: eslint compliance

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -6,38 +6,38 @@
 
 import kue from 'kue';
 
-//create a Kue queue
+// create a Kue queue
 const queue = kue.createQueue();
 
-//create an object containing job data
+// create an object containing job data
 const jobData = {
-    phoneNumber: '+254701285478',
-    message: 'This is the code to verify your account',
+  phoneNumber: '+254701285478',
+  message: 'This is the code to verify your account',
 };
 
-//create a job in the queue
+// create a job in the queue
 const job = queue.create('push_notification_code', jobData);
 
-//event listener for successful job creation
+// event listener for successful job creation
 job.on('complete', () => {
-    console.log('Notification job completed');
+  console.log('Notification job completed');
 });
 
 // event listerner for failed job
 job.on('failed', () => {
-    console.log('Notification job failed');
+  console.log('Notification job failed');
 });
 
-//save the job to the queue
+// save the job to the queue
 job.save((err) => {
-    if (!err) {
-        console.log(`Notification job created: ${job.id}`);
-    }
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+  }
 });
 
-//close queue after delay to allow time for job completion events to fire
+// close queue after delay to allow time for job completion events to fire
 setTimeout(() => {
-    queue.shutdown(5000, () => {
-        process.exit(0);
-    });
+  queue.shutdown(5000, () => {
+    process.exit(0);
+  });
 }, 1000);
